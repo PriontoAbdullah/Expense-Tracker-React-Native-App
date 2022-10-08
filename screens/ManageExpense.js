9d46245dada014d8ea621e1b344ef8c046132586
PriontoAbdullah <priontosoft@gmail.com>
2022-10-08 18:11:21 +0600

    Added Expense Form

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,5 +1,6 @@
 import { useContext, useLayoutEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
+import ExpenseForm from '../components/ManageExpense/ExpenseForm';
 
 import Button from '../components/UI/Button';
 import IconButton from '../components/UI/IconButton';
@@ -46,6 +47,7 @@ function ManageExpense({ route, navigation }) {
 
   return (
     <View style={styles.container}>
+      <ExpenseForm />
       <View style={styles.buttons}>
         <Button style={styles.button} mode='flat' onPress={cancelHandler}>
           Cancel
@@ -80,6 +82,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
+    marginTop: 20,
   },
   button: {
     minWidth: 120,
